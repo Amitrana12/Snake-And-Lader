6d28e4e755c205e87e121be75907f3d04ca21d09
amit <[amirana14325@gmail.com]>
2021-03-07 00:56:52 +0530

    [Amit Rana]Added::Starting position

--- SnakeAndLader/SnakeAndLader/Program.cs
+++ SnakeAndLader/SnakeAndLader/Program.cs
@@ -4,9 +4,14 @@ namespace SnakeAndLader
 {
     class Program
     {
+        public const int START_POINT = 0;
+        public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome To Snake And Lader Problem");
+            Console.WriteLine("Hello Welcome To Snake And Lader Problem \nEnter player name");
+            string player1 = Console.ReadLine();
+            int playerPosition = START_POINT;
+            Console.WriteLine("Current Position : " + playerPosition);
         }
     }
 }
